587013acdb47a4116949962aa1c622c6d52f4ff0
amandaesmith3 <amanda.s@ionic.io>
2022-05-20 09:19:31 -0500

    add nested popovers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonItem, IonLabel, IonList, IonPage, IonPopover } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonButton id="nested-button">Click to open popover</IonButton>
+        <IonPopover trigger="nested-button" dismissOnSelect={true}>
+          <IonContent>
+            <IonList>
+              <IonItem button={true} detail={false}>
+                <IonLabel>Option 1</IonLabel>
+              </IonItem>
+              <IonItem button={true} detail={false}>
+                <IonLabel>Option 2</IonLabel>
+              </IonItem>
+              <IonItem button={true} detail={true} id="nested-trigger">
+                <IonLabel>More options...</IonLabel>
+              </IonItem>
+
+              <IonPopover trigger="nested-trigger" dismissOnSelect={true} side="end">
+                <IonContent>
+                  <IonItem button={true}>
+                    <IonLabel>Nested Option</IonLabel>
+                  </IonItem>
+                </IonContent>
+              </IonPopover>
+            </IonList>
+          </IonContent>
+        </IonPopover>
       </IonContent>
     </IonPage>
   );
